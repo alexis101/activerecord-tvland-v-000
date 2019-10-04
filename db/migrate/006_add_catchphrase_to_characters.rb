1b606ed3bbe0218ad5ab820ea2fedfea1bda12fc
Alexis Kondilis <kondilisalexis@gmail.com>
2019-10-04 13:57:36 +0000

    Automatically backed up by Learn

--- db/migrate/006_add_catchphrase_to_characters.rb
+++ db/migrate/006_add_catchphrase_to_characters.rb
@@ -1,4 +1,4 @@
-class AddCatchPhraseToCharacters < ActiveRecord::Migration[5.1]
+class AddCatchPhrase < ActiveRecord::Migration[5.1]
   def change
     add_column :characters, :catchphrase, :string
   end
